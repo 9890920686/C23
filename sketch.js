7bffff5e7ee526b822310a3d89c37f563cc99d3d
9890920686 <72739901+9890920686@users.noreply.github.com>
2020-11-25 16:57:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+const Engine=Matter.Engine;
+const World= Matter.World;
+const Bodies=Matter.Bodies;
+
+
+var eng,wld;
+var box1,box2;
+var ground1;
+
+
+
+
+function setup() {
+  createCanvas(800,500);
+  eng=Engine.create();
+  wld=eng.world;
+
+  box1= new Box(200,300,50,50)
+  box2= new Box(240,100,50,100)
+
+  ground1 = new Ground(400,400,800,10)
+  
+}
+
+function draw() {
+  background(0);  
+  Engine.update(eng)
+
+  box1.display();
+  box2.display();
+
+  ground1.display();
+  
+  
+}
